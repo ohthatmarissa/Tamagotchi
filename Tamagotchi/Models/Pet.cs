8f93602ad05509e295fea608cb6a6f8ab69e339e
Kevin Garvey and Marissa Perry <example@example.com>
2019-05-01 15:20:10 -0700

    add additional methods to pet object

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -5,12 +5,20 @@ namespace Tamagotchi.Models
   public class Pet
   {
     private string _name;
+    private int _food;
+    private int _happiness;
+    private int _rest;
+    private bool _alive;
     private int _id;
     private static List<Pet> _instances = new List<Pet> {};
 
     public Pet (string name)
     {
       _name = name;
+      _food = 10;
+      _happiness = 10;
+      _rest = 10;
+      _alive = true;
       _instances.Add(this);
       _id = _instances.Count;
     }
@@ -19,17 +27,31 @@ namespace Tamagotchi.Models
     {
       return _instances;
     }
+    public static Pet Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+
     public int GetId()
     {
       return _id;
     }
+    public int GetFood()
+    {
+      return _food;
+    }
+    public int GetHappiness()
+    {
+      return _happiness;
+    }
+    public int GetRest()
+    {
+      return _rest;
+    }
     public string GetName()
     {
       return _name;
-    }
-    public static Pet Find(int searchId)
-    {
-      return _instances[searchId-1];
     }
   }
 }
